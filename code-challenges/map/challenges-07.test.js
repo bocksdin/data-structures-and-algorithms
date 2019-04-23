9346115d022ff3a35b8e04925154c650b6ce579b
RoryLetteney <rory.letteney@gmail.com>
2019-04-23 17:17:14 -0500

    Challenge 4 passed.

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -48,9 +48,7 @@ Read the MDN documentation on String.charCodeAt() if necessary.
 For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
-const charCode = (arr) => {
-  // Solution code here...
-};
+const charCode = (arr) => arr.map(char => char.charCodeAt(0));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
