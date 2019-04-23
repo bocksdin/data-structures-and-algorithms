b465a9a5f48a02ba2c353a72b3890a94396403c3
RoryLetteney <rory.letteney@gmail.com>
2019-04-23 17:11:22 -0500

    Challenge 2 passed.

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -25,7 +25,9 @@ Write a function named forEachTwoToThe that produces the same output as your for
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  // Solution code here...
+  let result = [];
+  arr.forEach(num => result.push(2 ** num));
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
