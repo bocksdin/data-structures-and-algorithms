3925a6f020a22f41d2f64962fbbfe182b3bbb7a1
RoryLetteney <rory.letteney@gmail.com>
2019-05-01 15:18:10 -0500

    Challenge 7 passed.

--- code-challenges/string-methods/challenges-13.test.js
+++ code-challenges/string-methods/challenges-13.test.js
@@ -62,9 +62,7 @@ CHALLENGE 7
 Write a function named findEvery that takes in an array of strings, along with a target string. Return a Boolean based on whether or not every string in the array contains the target string.
 ------------------------------------------------------------------------------------------------ */
 
-const findEvery = (arr, target) => {
-  // Solution code here...
-};
+const findEvery = (arr, target) => arr.every(str => str.includes(target));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8
