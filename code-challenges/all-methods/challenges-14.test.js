fcc400c520486f4b9892d276c92aadc1637a803a
RoryLetteney <rory.letteney@gmail.com>
2019-05-02 15:36:22 -0500

    Challenge 1 passed.

--- code-challenges/all-methods/challenges-14.test.js
+++ code-challenges/all-methods/challenges-14.test.js
@@ -8,9 +8,7 @@ Write a function named toTitleCase that takes in an array of strings and returns
 For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyver'].
 ------------------------------------------------------------------------------------------------ */
 
-const toTitleCase = (arr) => {
-  // Solution code here...
-};
+const toTitleCase = (arr) => arr.map(str => str.charAt(0).toUpperCase() + str.slice(1));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
