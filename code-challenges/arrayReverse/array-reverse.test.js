ae5a3e7dc85a1c3b5012c900cdd3e1cea3b13485
RoryLetteney <rory.letteney@gmail.com>
2019-05-20 15:05:02 -0500

    Stretch goal complete

--- code-challenges/arrayReverse/array-reverse.test.js
+++ code-challenges/arrayReverse/array-reverse.test.js
@@ -1,11 +1,33 @@
 'use strict';
 
+// const reverseArray = arr => {
+//   if (Array.isArray(arr)) {
+//     let result = [];
+//     if (arr.length) {
+//       for (let i = 0; i < arr.length; i++) {
+//         result[i] = arr[arr.length - i - 1];
+//       }
+//     }
+//     return result;
+//   } else {
+//     return 'Not an array!';
+//   }
+// };
+
 const reverseArray = arr => {
   if (Array.isArray(arr)) {
     let result = [];
     if (arr.length) {
       for (let i = 0; i < arr.length; i++) {
-        result[i] = arr[arr.length - i - 1];
+        if (i === arr.length / 2) {
+          break;
+        } else {
+          let front = arr[i];
+          let back = arr[arr.length - i - 1];
+          arr[i] = back;
+          arr[arr.length - i - 1] = front;
+          result = arr;
+        }
       }
     }
     return result;
