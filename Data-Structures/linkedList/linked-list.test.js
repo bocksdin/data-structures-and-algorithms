5e890518bc0bce62100befc038caa6daf497ea47
RoryLetteney <rory.letteney@gmail.com>
2019-05-24 14:33:31 -0500

    Testing that includes returns true when the passed value exists within the linked list.

--- Data-Structures/linkedList/linked-list.test.js
+++ Data-Structures/linkedList/linked-list.test.js
@@ -5,10 +5,12 @@ const LinkedList = require('./linked-list');
 describe('A Singly Linked List', () => {
   const testList = new LinkedList();
   let value = 5;
+  let expected;
+  let received;
 
   it('should create an empty singly linked list', () => {
 
-    const expected = null;
+    expected = null;
 
     expect(testList.head).toStrictEqual(expected);
   });
@@ -16,29 +18,40 @@ describe('A Singly Linked List', () => {
   it('should properly insert a new node at the head', () => {
     testList.insert(value);
     
-    const received = testList.toString();
-    const expected = value.toString();
+    received = testList.toString();
+    expected = value.toString();
 
     expect(received).toBe(expected);
   });
 
   it('head should properly point to the first node in the linked list', () => {
-    const received = testList.head.value;
-    const expected = value;
+    received = testList.head.value;
+    expected = value;
 
     expect(received).toBe(expected);
   });
 
   it('can properly insert multiple nodes into the linked list', () => {
-    let expected = [value]
+    expected = [value]
     for (let i = 2; i < 4; i++) {
       value *= i;
       testList.insert(value);
       expected.push(value);
     }
 
-    const received = testList.toString();
+    received = testList.toString();
     expected = expected.reverse().join(', ');
+
+    expect(received).toBe(expected);
+  });
+
+  it('will return true when finding a value within the linked list that exists', () => {
+    value = expected.split(', ')
+      .reverse()[0];
+    value = parseInt(value);
+    
+    received = testList.includes(value);
+    expected = true;
 
     expect(received).toBe(expected);
   });
