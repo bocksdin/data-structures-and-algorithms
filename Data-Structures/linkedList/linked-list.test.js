6d10d9e4e59e4c67fe75b2da4a345d6875320176
RoryLetteney <rory.letteney@gmail.com>
2019-05-24 14:03:51 -0500

    Testing to make sure the head is the first node in the linked list.

--- Data-Structures/linkedList/linked-list.test.js
+++ Data-Structures/linkedList/linked-list.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const LinkedList = require('./linked-list');
+const value = 5;
 
 describe('A Singly Linked List', () => {
   const testList = new LinkedList();
@@ -12,13 +13,19 @@ describe('A Singly Linked List', () => {
     expect(testList.head).toStrictEqual(expected);
   });
 
-  it.only('should properly insert a new node at the head', () => {
-    const value = 5;
-    testList.insert(5);
+  it('should properly insert a new node at the head', () => {
+    testList.insert(value);
     
     const received = testList.toString();
     const expected = value.toString();
 
     expect(received).toBe(expected);
   });
+
+  it('head should properly point to the first node in the linked list', () => {
+    const received = testList.head.value;
+    const expected = value;
+
+    expect(received).toBe(expected);
+  })
 });
