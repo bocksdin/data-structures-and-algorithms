9901163ff4ad8a96659a50221fbab45ef735a6f2
RoryLetteney <rory.letteney@gmail.com>
2019-05-24 13:09:25 -0500

    Built insert method and adding a new node to the head.

--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -4,6 +4,12 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insert(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+    }
+  }
 }
 
 class Node {
