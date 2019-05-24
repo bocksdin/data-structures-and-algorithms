0c59c44506dfa8ffdd313dd26937d423a5250567
RoryLetteney <rory.letteney@gmail.com>
2019-05-24 13:59:20 -0500

    Testing for proper insertion of new nodes at the head.

--- Data-Structures/linkedList/linked-list.test.js
+++ Data-Structures/linkedList/linked-list.test.js
@@ -3,11 +3,22 @@
 const LinkedList = require('./linked-list');
 
 describe('A Singly Linked List', () => {
+  const testList = new LinkedList();
+
   it('should create an empty singly linked list', () => {
-    const emptyList = new LinkedList();
 
     const expected = null;
 
-    expect(emptyList.head).toStrictEqual(expected);
+    expect(testList.head).toStrictEqual(expected);
+  });
+
+  it.only('should properly insert a new node at the head', () => {
+    const value = 5;
+    testList.insert(5);
+    
+    const received = testList.toString();
+    const expected = value.toString();
+
+    expect(received).toBe(expected);
   });
 });
