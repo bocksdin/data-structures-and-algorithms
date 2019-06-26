b4182d7a09bdadd05f820e9f3b44939bf9d8641d
RoryLetteney <rory.letteney@gmail.com>
2019-06-26 14:04:54 -0500

    Changed final test to test the limits of an arbitrary right number.

--- code-challenges/quickSort/quick-sort.test.js
+++ code-challenges/quickSort/quick-sort.test.js
@@ -8,7 +8,7 @@ describe('A quickSort algorithm', () => {
     [quickSort([10], 0, 0), [10]],
     [quickSort([10, 1], 0, 1), [1, 10]],
     [quickSort([10, 1, 5], 0, 2), [1, 5, 10]],
-    [quickSort([10, 1, 5, 8, 10, 23, 255], 0, 6), [1, 5, 8, 10, 10, 23, 255]]
+    [quickSort([10, 1, 5, 8, 10, 23, 255], 0, 3), [1, 5, 8, 10, 10, 23, 255]]
   ])(
     '%s should be %s',
     (received, expected) => {
