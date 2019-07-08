d48c7cac37b772185bc254740bd03237293763d7
RoryLetteney <rory.letteney@gmail.com>
2019-07-08 16:51:25 -0500

    Happy path passing. Finding first duplicate word in the strings.

--- code-challenges/repeatedWord/repeated-word.js
+++ code-challenges/repeatedWord/repeated-word.js
@@ -4,4 +4,19 @@ const Hashtable = require('../../Data-Structures/hashtable/hashtable');
 
 module.exports = str => {
   if (!str || typeof str !== 'string') return 'Please provide a string!';
+
+  let hashtable = new Hashtable;
+  let firstDup = null;
+
+  str.split(' ').forEach(word => {
+    word = word.toLowerCase().replace(',', '');
+    if (!firstDup && hashtable.contains(word)) {
+      firstDup = word;
+    }
+    let value = {};
+    value[word] = word;
+    hashtable.add(value);
+  });
+
+  return { firstDup };
 };
